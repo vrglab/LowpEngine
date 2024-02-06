80dd910c0f15ac49e85a6fa6af2cc3ad2fc5241c
Vrglab <vrglab1@gmail.com>
2024-02-06 13:58:54 +0100

    better debuging for Scripting engine

--- ScriptingEngine/ScriptingEngine.cpp
+++ ScriptingEngine/ScriptingEngine.cpp
@@ -1,13 +1,15 @@
 #include "pch.h"
 #include "ScriptingEngine.h"
+#include <Core/Debugging/Debug.h>
 
 void ScriptingEngine::InitMono()
 {
+    LP_CORE_INFO("Starting Scripting engine");
 	mono_set_dirs("Mono/MonoBleedingEdge", "Mono/configs");
 	mono_config_parse(nullptr);
     monoDomain = mono_jit_init("GameEngineDomain");
     if (!monoDomain) {
-        std::cerr << "Failed to initialize Mono JIT." << std::endl;
+        LP_CORE_ERROR("Failed to initialize Mono JIT.");
         return;
     }
 }
