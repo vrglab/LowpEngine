7935bd7b2c53ec53f8c245f4f3120d2b4fd36c1c
Vrglab <vrglab1@gmail.com>
2024-02-23 19:48:11 +0100

    Linux Update
    GNR:
    1.Fixed Update project file

--- UpdatePackages.bat
+++ UpdatePackages.bat
@@ -1,2 +1,3 @@
+call Programs\vcpkg\vcpkg update
 call Programs\vcpkg\vcpkg upgrade --no-dry-run
 call GenerateProjectFiles.bat
--- UpdatePackages.sh
+++ UpdatePackages.sh
@@ -1,2 +1,3 @@
+sh Programs/vcpkg/vcpkg update
 sh Programs/vcpkg/vcpkg upgrade --no-dry-run
 sh GenerateProjectFiles.sh
